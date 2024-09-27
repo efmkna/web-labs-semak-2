d84d5a5ecf4dbe6860499359da639ea31d744556
ефимкина софья <sonya_kov@icloud.com>
2024-09-28 00:47:14 +0700

    меню

--- app.py
+++ app.py
@@ -14,8 +14,12 @@ def start():
         <header>
             НГТУ, ФБ, лаба 1
         </header>
-
+        
         <h1>web-сервер на Flask</h1>
+
+        <ul>
+            <li><a href="/lab1">первая лаба</a></li>
+        </ul>
 
         <footer>
             &copy; Ефимкина Софья, ФБИ-24, 3 курс, 2024
@@ -23,3 +27,26 @@ def start():
     </body>
 </html>
 """
+
+@app.route("/lab1")
+def lab1():
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>первая лаба</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, первая лаба
+        </header>
+
+        <h1>Содержимое первой лабораторной</h1>
+
+        <footer>
+            &copy; Ефимкина Софья, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
