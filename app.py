c8958205add03bfb849baa19fa08189f769f0b24
ефимкина софья <sonya_kov@icloud.com>
2024-09-28 01:07:17 +0700

    статические файлы

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, url_for
 app = Flask(__name__)
 
 @app.route("/")
@@ -53,3 +53,18 @@ def menu():
     </body>
 </html>
 """
+
+@app.route("/lab1/oak")
+def oak(): 
+    return'''
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="''' + url_for('static', filename='lab1.css') + '''">
+    </head>
+    <body>
+        <h1>дуб</h1>
+        <img src="''' + url_for('static', filename='oak.jpg') + '''">
+    </body>
+</html>
+'''
