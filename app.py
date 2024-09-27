67f67ccb066cebf6d7aea5626fc3e64cfc2293a3
ефимкина софья <sonya_kov@icloud.com>
2024-09-28 00:52:55 +0700

    перенаправление

--- app.py
+++ app.py
@@ -1,10 +1,36 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 @app.route("/index")
 def start():
+    return redirect("/menu", code=302)
+
+@app.route("/lab1")
+def lab1():
     return """
+<!doctype html>
+<html>
+    <head>
+        <title>первая лаба</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, первая лаба
+        </header>
+
+        <h1>содержимое первой лабы</h1>
+
+        <footer>
+            &copy; Ефимкина Софья, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
+
+@app.route("/menu")
+def menu():
+     return """
 <!doctype html>
 <html>
     <head>
@@ -27,26 +53,3 @@ def start():
     </body>
 </html>
 """
-
-@app.route("/lab1")
-def lab1():
-    return """
-<!doctype html>
-<html>
-    <head>
-        <title>первая лаба</title>
-    </head>
-    <body>
-        <header>
-            НГТУ, ФБ, первая лаба
-        </header>
-
-        <h1>Содержимое первой лабораторной</h1>
-
-        <footer>
-            &copy; Ефимкина Софья, ФБИ-24, 3 курс, 2024
-        </footer>
-    </body>
-</html>
-"""
-
