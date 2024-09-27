dd735175bba035784b43491c25a662c3f45711c6
ефимкина софья <sonya_kov@icloud.com>
2024-09-27 23:49:17 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -3,4 +3,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def start():
-    return "web-сервер на flask"
+    return """
+<!doctype html>
+<html>
+    <head>
+        <title>Ефимкина Софья Алексеевна, лаба 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, лаба 1
+        </header>
+
+        <h1>web-сервер на Flask</h1>
+
+        <footer>
+            &copy; Ефимкина Софья, ФБИ-24, 3 курс, 2024
+        </footer>
+    </body>
+</html>
+"""
